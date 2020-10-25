078c9781e7234e3202eea4bbf1ed381bc22766a2
Mark Snyder <marksnyder423@gmail.com>
2020-10-25 12:02:15 -0500

    End of section 3 API Architecture

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -23,21 +24,28 @@ namespace API
         public Startup(IConfiguration config)
         {
             _config = config;
-
         }
 
         // We commented out.  Cummings prefers different approach
         // public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // Ordering doesn't matter
         public void ConfigureServices(IServiceCollection services)
         {
+            // Lifetimes of services
+            // Transient - short life.  Created and destroyed at method level
+            // Singleton - created at application start and destroyed when application ends
+            // Scoped - 
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddControllers();
+            // Uses Scoped lifetime
             services.AddDbContext<StoreContext>(x => 
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // Ordering is very important
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
